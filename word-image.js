df122bdca673bd0c7355f498ed55b914a3da3a70
LukeTheNeko <lucasneko25@gmail.com>
2024-03-30 19:50:14 -0300

    v1.2.0

---
+++ word-image.js
@@ -0,0 +1,77 @@
+const { createCanvas, loadImage } = require("@napi-rs/canvas");
+const fs = require("fs");
+require("dotenv").config({ path: __dirname + "/.env" });
+const { twitterClient } = require("./twitterClient.js");
+const path = require("path");
+
+const getRandomWord = () => {
+    return "Teste";
+};
+
+const generateImage = async (word) => {
+    const canvas = createCanvas(800, 400);
+    const ctx = canvas.getContext("2d");
+
+    const backgroundFiles = fs.readdirSync("backgrounds/");
+    const randomIndex = Math.floor(Math.random() * backgroundFiles.length);
+    const randomBackground = backgroundFiles[randomIndex];
+
+    const background = await loadImage(`backgrounds/${randomBackground}`);
+    ctx.filter = "blur(6px)";
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    ctx.filter = "blur(0px)";
+    ctx.font = "bold 85px Arial";
+    ctx.fillStyle = "#000000";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+
+    const textX = canvas.width / 2;
+    const textY = canvas.height / 2;
+
+    const borderWidth = 2;
+    ctx.lineWidth = borderWidth;
+    ctx.strokeStyle = "#ffffff";
+
+    for (let offsetX = -borderWidth; offsetX <= borderWidth; offsetX++) {
+        for (let offsetY = -borderWidth; offsetY <= borderWidth; offsetY++) {
+            ctx.strokeText(word, textX + offsetX, textY + offsetY);
+        }
+    }
+
+    ctx.fillText(word, textX, textY);
+
+    return canvas.toBuffer("image/png");
+};
+
+const tweetWithImage = async () => {
+    try {
+        const randomWord = getRandomWord();
+
+        const imageBuffer = await generateImage(randomWord);
+
+        const tempImagePath = path.join(__dirname, "img.png");
+        fs.writeFileSync(tempImagePath, imageBuffer);
+
+        const mediaId = await twitterClient.v1.uploadMedia(tempImagePath);
+
+        await twitterClient.v2.tweet({
+            text: `${randomWord}`,
+            media: { media_ids: [mediaId] },
+        });
+
+        console.log(`Tweet with Word "${randomWord}" + image successfully sent`);
+
+        fs.unlinkSync(tempImagePath);
+    } catch (error) {
+        console.error("Error sending tweet with image:", error);
+    }
+};
+
+const tweetEvery10Minutes = () => {
+    tweetWithImage();
+
+    setInterval(tweetWithImage, 600000);
+};
+
+tweetEvery10Minutes();
